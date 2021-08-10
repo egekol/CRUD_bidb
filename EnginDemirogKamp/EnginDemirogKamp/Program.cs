247c1d0e5017567dcaa9d7ff4fe845482e90fce8
Ege Kol <kol18@itu.edu.tr>
2021-08-10 18:26:29 +0300

    C# temelleri part 2

--- EnginDemirogKamp/EnginDemirogKamp/Program.cs
+++ EnginDemirogKamp/EnginDemirogKamp/Program.cs
@@ -6,7 +6,37 @@ namespace EnginDemirogKamp
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello World!");
+
+            Kurs kurs1 = new Kurs();
+            kurs1.IzlenmeOrani = 68;
+            kurs1.KursAdi = "C#";
+
+            Kurs kurs2 = new Kurs();
+            kurs2.IzlenmeOrani = 40;
+            kurs2.KursAdi = "Java";
+
+            Kurs kurs3 = new Kurs();
+            kurs3.IzlenmeOrani = 80;
+            kurs3.KursAdi = "Python";
+
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2, kurs3};
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi);
+            }
         }
+    }
+
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+
+        public int IzlenmeOrani { get; set; }
+        
+
     }
 }
---
+++ EnginDemirogKamp/Metotlar/Urun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class Urun
+    {
+
+        //isimlendirmeler ingilizce olmalı
+
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+
+    }
+}
